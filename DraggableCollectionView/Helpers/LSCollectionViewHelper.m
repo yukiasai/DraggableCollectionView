c0d24f4ecbcbc2ef6dd0a17de51e9234166c2ea6
yukiasai <yukiasai@gmail.com>
2016-09-26 10:51:13 +0900

    Cells reordering dose not work in iOS10.

--- DraggableCollectionView/Helpers/LSCollectionViewHelper.m
+++ DraggableCollectionView/Helpers/LSCollectionViewHelper.m
@@ -67,7 +67,7 @@ typedef NS_ENUM(NSInteger, _ScrollingDirection) {
         [_collectionView addGestureRecognizer:_panPressGestureRecognizer];
         
         for (UIGestureRecognizer *gestureRecognizer in _collectionView.gestureRecognizers) {
-            if ([gestureRecognizer isKindOfClass:[UILongPressGestureRecognizer class]]) {
+            if ([gestureRecognizer isKindOfClass:[UILongPressGestureRecognizer class]] && gestureRecognizer != _longPressGestureRecognizer) {
                 [gestureRecognizer requireGestureRecognizerToFail:_longPressGestureRecognizer];
                 break;
             }
